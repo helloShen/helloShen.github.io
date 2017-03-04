2134a7b642cc98b5c9e7c16d3a4aa75da67b25e1
helloShen <symantec__@hotmail.com>
2017-03-04 18:05:55 -0500

    correct build.sh process

--- build.sh
+++ build.sh
@@ -1,3 +1,19 @@
+##################################################################
+#
+# 此文件用来重新生成网站，提交至本地Git仓库。
+# 注意！所有的提交都在jekyll_beta分支上。
+# 而且，并没有推送至Github上。要部署至Github需要运行deploy.sh。
+#
+# 命令格式如下：
+#
+# 	sh build.sh <discription>
+#
+# 需要一个参数<discription>，用来简短描述此次提交的修改项。
+# 注意，描述语句中间不能有空格。
+#
+##################################################################
+
+
 # 生成所有categories页面
 javac -d /Users/Wei/github/ciaoshen/java/com/ciaoshen/blog/bin/ /Users/Wei/github/ciaoshen/java/com/ciaoshen/blog/src/CategoriesPageGenerator.java
 java -classpath /Users/Wei/github/ciaoshen/java/com/ciaoshen/blog/bin/ com.ciaoshen.blog.CategoriesPageGenerator
@@ -9,4 +25,8 @@ java -classpath /Users/Wei/github/ciaoshen/java/com/ciaoshen/blog/bin/ com.ciaos
 # 重新生成_site
 jekyll build
 
-
+# 提交至Github
+# $1: 更新描述，比如增加了哪篇文章
+git checkout jekyll_beta
+git add .
+git commit -m $1
--- new.sh
+++ new.sh
@@ -1,3 +1,15 @@
+###################################################################
+#
+# 此文件用来自动按照当地时间，生成一个带有默认YAML头的.md文件
+# 命令格式如下：
+#
+# 	sh new.sh <post_name>
+#
+# 注意post_name需要带有扩展名，比如.md表示markdown文件。
+#
+###################################################################
+
+
 # 运行PostGenerator，以生成带YAML头区块的新markdown文件
 # 参数：$1 代表文件名。例如，newFile.md。 注意！文件名需要带后缀
 javac -d /Users/Wei/github/ciaoshen/java/com/ciaoshen/blog/bin/ /Users/Wei/github/ciaoshen/java/com/ciaoshen/blog/src/PostGenerator.java
