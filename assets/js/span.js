1f50a3dda709b71d7e18fdbfb5f0a11fd2339a6f
helloShen <symantec__@hotmail.com>
2017-03-09 14:17:50 -0500

    add visitor statistic module

--- assets/js/span.js
+++ assets/js/span.js
@@ -1,6 +1,8 @@
-$(window).load(function(){	//控制段落空行
-	$("h3").before("</br></br>");
-	$("h3").after("<hr>");
+// 我的Markdown里，h3是一级标题
+// 前后加空行，并换字体
+$(window).load(function(){
 	$("h3").addClass("lead");
+	$("h3.lead").before("</br>");
+	$("h3.lead").after("<hr>");
 	$("h4").before("</br>");
 })
